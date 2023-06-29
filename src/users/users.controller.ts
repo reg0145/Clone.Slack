0c8c349cdec93d650eafa649ce6196292eb3ee0b
donghyuk <reg0145@naver.com>
2023-06-29 22:30:16 +0900

    [Fact] Users API 함수 만들기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,11 +3,11 @@ import { Body, Controller, Get, Post, Res } from "@nestjs/common";
 @Controller('users')
 export class UsersController {
   @Get()
-  getUesrs(){}
+  getUsers(){}
   @Post()
   createUser(@Body('email') email, @Body('nickname') nickname, @Body('password') password) {}
   @Post('login')
-  login(@Body('email') email, @Body('password') password)
+  login(@Body('email') email, @Body('password') password) {}
   @Post('logout')
   logout(@Res() res){
     res.logout();
