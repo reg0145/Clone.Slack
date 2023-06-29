3e9f018d4d0ea761a503029189b429b8a7b7c605
donghyuk <reg0145@naver.com>
2023-06-29 20:00:16 +0900

    [Feat] Channels 모듈 만들기

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule, ChannelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
