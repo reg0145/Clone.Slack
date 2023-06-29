c4766feb4d3ff7838fdfa7a8daf6d40c2e7d2eb4
donghyuk <reg0145@naver.com>
2023-06-29 20:47:07 +0900

    [Feat] DMS API 함수 만들기

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
-@Controller('dms')
-export class DmsController {}
+@Controller('/workspaces/:workspace/dms')
+export class DmsController {
+  @Get(':id/chats')
+  getChats(@Param('perPage') perPage, @Param('number') page){}
+  @Get(':id/unreads')
+  getUnReads(@Param('after') after){}
+  @Post(':id/chats')
+  saveChats(@Body('content') content) {}
+  @Post(':id/images')
+  saveImage(@Body('image') image){}
+}
