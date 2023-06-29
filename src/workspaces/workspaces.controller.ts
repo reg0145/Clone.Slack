d30f0bddac2816095e7d49f47e448011a8be8c29
donghyuk <reg0145@naver.com>
2023-06-29 20:39:29 +0900

    [Feat] WorkSpace API 함수 만들기

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('workspaces')
-export class WorkspacesController {}
+export class WorkspacesController {
+  @Get()
+  getAllWorkspace(){}
+  @Post()
+  addWorkspace(@Body('workspace') workspace, @Body('url') url){}
+  @Get(':workspace/members')
+  getAllMemberInWorkspace() {}
+  @Post(':workspace/members')
+  inviteMemgerFromWorkspace(@Body('email') email) {}
+  @Get(':workspace/users/:id')
+  getMemberInWorkspace(){}
+  @Delete(':workspace/members/:id')
+  deleteMemberInWorkspace(@Param('id') id) {}
+}
