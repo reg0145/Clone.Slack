9ea49534b0888183bb00286479475d89b397932b
donghyuk <reg0145@naver.com>
2023-06-30 10:11:44 +0900

    [Fact] Swagger 설치 및 기본 설정

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
@@ -7,6 +8,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 
+  const config = new DocumentBuilder()
+    .setTitle('Slack API')
+    .setDescription('Slack 개발을 위한 API입니다.')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
