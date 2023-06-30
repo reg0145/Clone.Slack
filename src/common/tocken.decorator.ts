3c7cdb2460cb9bb18bc823085fac3c2b99857547
donghyuk <reg0145@naver.com>
2023-06-30 10:41:21 +0900

    [Fact] Token 데코레이터 만들기

---
+++ src/common/tocken.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Token = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const response = ctx.switchToHttp().getResponse();
+    return response.getToken();
+  },
+);
